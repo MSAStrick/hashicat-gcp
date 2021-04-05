cf96914217d5635996e6c1a3eeb91e52c1b137c0
Tim Strickland <timothy.strickland@amfam.com>
2021-04-05 20:31:02 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,16 @@
-module "network_vpc" {
-  source  = "app.terraform.io/TimS-training/network/google//modules/vpc"
+module "network" {
+  source  = "app.terraform.io/bevish-training/network/google"
   version = "2.5.0"
 
+  project_id   = var.project
+  network_name = "gaurav-network"
+  routing_mode = "GLOBAL"
+
+  subnets = [
+    {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+    }
+  ]
 }
-
-    project_id   = var.project
-    network_name = "gaurav-network"
-    routing_mode = "GLOBAL"
-
-    subnets = [
-        {
-            subnet_name           = "gaurav-subnet"
-            subnet_ip             = "10.100.10.0/24"
-            subnet_region         = var.region
-        }
-    ]
-  }
