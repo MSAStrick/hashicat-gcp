337df0d327a63de1a2e9ab305aec1a3f729c00f7
Tim Strickland <timothy.strickland@amfam.com>
2021-04-05 20:10:02 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -6,10 +6,11 @@ module "vpc" {
     network_name = "gaurav-network"
     routing_mode = "GLOBAL"
 
-subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
+    subnets = [
+        {
+            subnet_name           = "gaurav-subnet"
+            subnet_ip             = "10.100.10.0/24"
+            subnet_region         = "var.region"
+        }
+    ]
   }
-]
